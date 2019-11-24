53fa87cc674829694f016daadfa6ca4ce5855718
Aditya Vinod Kumar <ads.vinodk@gmail.com>
2019-11-24 09:22:06 +0530

    Added CNN Test and Zarr usability

--- extract_labels.py
+++ extract_labels.py
@@ -5,52 +5,87 @@ import time
 from PIL import Image
 from imutils import paths
 from tqdm import tqdm
-from multiprocessing import Manager, Process, Pool
-
-with open('train_annotations.json', 'r') as file:
-    train_annotations = json.load(file)
-
-with open('validation_annotations.json', 'r') as file:
-    validation_annotations = json.load(file)
 
 target_size = (128,128)
 
-trainImagePaths = list(paths.list_images('train'))
-shape = (len(trainImagePaths),)+target_size+(3,)
-train_x = np.zeros(shape=shape, dtype=np.float16)
-train_y = []
-for i in tqdm(range(len(trainImagePaths))):
-    imagePath = trainImagePaths[i]
-    image = Image.open(imagePath)
-    image = image.resize(size=target_size, resample=Image.LANCZOS)
-    image = np.array(image)
-    if (len(image.shape) == 2):
-        image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
-    train_x[i]=image
-    labels = set()
-    for category in train_annotations[imagePath.split(os.path.sep)[-1]]['categories']: labels.add(category)
-    train_y.append(list(labels))
+if 'train_x_128.npy' not in os.listdir('.'):
+    with open('train_annotations.json', 'r') as file:
+        train_annotations = json.load(file)
 
-train_y = np.array(train_y)
-np.save('train_x_128.npy', train_x)
-np.save('train_y.npy', train_y)
+    print("Storing Train Images into Numpy Array format...")
+    trainImagePaths = list(paths.list_images('train'))
+    shape = (len(trainImagePaths),)+target_size+(3,)
+    train_x = np.zeros(shape=shape, dtype=np.float16)
+    train_y = []
+    for i in tqdm(range(len(trainImagePaths))):
+        imagePath = trainImagePaths[i]
+        image = Image.open(imagePath)
+        image = image.resize(size=target_size, resample=Image.LANCZOS)
+        image = np.array(image)
+        if (len(image.shape) == 2):
+            image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+        train_x[i]=image
+        labels = set()
+        for category in train_annotations[imagePath.split(os.path.sep)[-1]]['categories']: labels.add(category)
+        train_y.append(list(labels))
 
-validationImagePaths = list(paths.list_images('validation'))
-shape = (len(validationImagePaths),)+target_size+(3,)
-validation_x = np.zeros(shape=shape, dtype=np.float16)
-validation_y = []
-for i in tqdm(range(len(validationImagePaths))):
-    imagePath = validationImagePaths[i]
-    image = Image.open(imagePath)
-    image = image.resize(size=target_size, resample=Image.LANCZOS)
-    image = np.array(image)
-    if (len(image.shape) == 2):
-        image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
-    validation_x[i]=image
-    labels = set()
-    for category in validation_annotations[imagePath.split(os.path.sep)[-1]]['categories']: labels.add(category)
-    validation_y.append(list(labels))
+    train_y = np.array(train_y)
+    np.save('train_x_128.npy', train_x)
+    np.save('train_y.npy', train_y)    
 
-validation_y = np.array(validation_y)
-np.save('validation_x_128.npy', validation_x)
-np.save('validation_y.npy', validation_y)
+    del train_annotations; del train_x; del train_y; del trainImagePaths; 
+
+if 'validation_x_128.npy' not in os.listdir('.'):
+    with open('validation_annotations.json', 'r') as file:
+        validation_annotations = json.load(file)
+
+    print("Storing Validation Images into Numpy Array format...")
+    validationImagePaths = list(paths.list_images('validation'))
+    shape = (len(validationImagePaths),)+target_size+(3,)
+    validation_x = np.zeros(shape=shape, dtype=np.float16)
+    validation_y = []
+    for i in tqdm(range(len(validationImagePaths))):
+        imagePath = validationImagePaths[i]
+        image = Image.open(imagePath)
+        image = image.resize(size=target_size, resample=Image.LANCZOS)
+        image = np.array(image)
+        if (len(image.shape) == 2):
+            image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+        validation_x[i]=image
+        labels = set()
+        for category in validation_annotations[imagePath.split(os.path.sep)[-1]]['categories']: labels.add(category)
+        validation_y.append(list(labels))
+
+    validation_y = np.array(validation_y)
+    np.save('validation_x_128.npy', validation_x)
+    np.save('validation_y.npy', validation_y)
+
+    del validation_annotations; del validation_x; del validation_y; del validationImagePaths; 
+
+if 'test_x_128.npy' not in os.listdir('.'):
+    with open('test_annotations.json', 'r') as file:
+        test_annotations = json.load(file)
+
+    print("Storing Test Images into Numpy Array format...")
+    testImagePaths = list(paths.list_images('test'))
+    shape = (len(testImagePaths),)+target_size+(3,)
+    test_x = np.zeros(shape=shape, dtype=np.float16)
+    test_y = []
+
+    for i in tqdm(range(len(testImagePaths))):
+        imagePath = testImagePaths[i]
+        image = Image.open(imagePath)
+        image = image.resize(size=target_size, resample=Image.LANCZOS)
+        image = np.array(image)
+        if (len(image.shape) == 2):
+            image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+        test_x[i]=image
+        labels = set()
+        for category in test_annotations[imagePath.split(os.path.sep)[-1]]['categories']: labels.add(category)
+        test_y.append(list(labels))
+
+    test_y = np.array(test_y)
+    np.save('test_x_128.npy', test_x)
+    np.save('test_y.npy', test_y)
+
+    del test_annotations; del test_x; del test_y; del testImagePaths; 
--- test_cnn_model.py
+++ test_cnn_model.py
@@ -1,38 +1,57 @@
-from tensorflow.keras.models import load_model
-from PIL import Image
-from imutils import paths
-import numpy as np
 import json
-import time
-import cv2
+import os
+import numpy as np
+import dask.array as da
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
+from sklearn.preprocessing import MultiLabelBinarizer
 
-model = load_model('cnn_model.h5')
+labels = da.from_zarr('labels.zarr')
 
-with open('annotations/instances_train2014.json','r') as file:
-    instances = json.load(file)
+target_size = (128,128)
+test_data_dir = 'test'
+nb_test_samples = len(os.listdir(test_data_dir))
+epochs = 50
+batch_size = 60
 
-labels = np.array([category['name'] for category in instances['categories']])
-del instances
+if "best_model.h5" in os.listdir():
+    model = load_model("best_model.h5")
 
-with open('test_annotations.json','r') as file:
-    test_annotations = json.load(file)
+if "test_x_128_dask.zarr" in os.listdir('.') and "test_y_dask.zarr" in os.listdir('.'):
+    with tf.device('/cpu:0'):
+        test_x = da.from_zarr("test_x_128_dask.zarr")
+        test_y = da.from_zarr("test_y_dask.zarr")
 
-file_name = 'test/COCO_train2014_000000527649.jpg'
-img = Image.open(file_name)
-img = img.resize((128, 128), Image.ANTIALIAS)
-img_arr = np.array(img)
-img_arr = np.expand_dims(img_arr, 0)
-img_arr = img_arr/255
-proba = model.predict(img_arr)[0]
-proba= (proba*1000).astype(int)
-print(proba)
-print("Predicted Classes -")
-for i in range(len(proba)):
-    if proba[i]>50:
-        print(labels[i])
-print("-------------------------")
+else: 
+    test_x = np.load('test_x_128.npy', allow_pickle=True)
+    test_x = test_x.astype('uint8')
+    test_x = da.from_array(test_x, chunks = (827,128,128,3))
+    test_x.to_zarr("test_x_128_dask.zarr")
+    
+    test_y = np.load('test_y.npy', allow_pickle=True)
+    test_y = np.array(test_y)
+    
+    mlb = MultiLabelBinarizer(labels)
+    test_y = mlb.fit_transform(test_y)
+    
+    test_y = test_y.astype('uint8')
+    test_y = da.from_array(test_y, chunks = (827,128,128,3))
+    test_y.to_zarr("test_y_128_dask.zarr")
 
-actual = [category for category in list(test_annotations[file_name.split('/')[1]]['categories'].keys())]
-print("Actual Classes -")
-for name in actual:
-    print(name)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+with tf.device('/gpu:0'):
+    stop_early = EarlyStopping(monitor='val_loss',patience = 20)
+    reduceLR = ReduceLROnPlateau(monitor='val_loss',paitence = 20, factor=0.2, min_lr = 0.0001)
+    ModelCheck = ModelCheckpoint('best_model_loss.h5', monitor='val_loss', mode='min', verbose=1, save_best_only=True)
+    callbacks = [stop_early, reduceLR, ModelCheck]
+    model.evaluate_generator(
+        test_datagen.flow(test_x, test_y, batch_size=batch_size),
+        steps = nb_test_samples//batch_size,
+        callbacks = callbacks,
+        use_multiprocessing=True
+    )
