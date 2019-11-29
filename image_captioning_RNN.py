9e3117184547f22abb9a219f9bea9448b92e06af
Sagar R. Garg <sagarratangarg@gmail.com>
2019-11-29 23:22:56 +0530

    Final Project 29/11/2019

---
+++ image_captioning_RNN.py
@@ -0,0 +1,96 @@
+import json as js
+from tqdm import tqdm
+import string
+import sys
+import keras
+import tensorflow as tf
+import json
+import numpy as np
+import dask.array as da
+import math
+import os
+# os.environ["CUDA_DEVICE_ORDER"]="0"
+# os.environ["CUDA_VISIBLE_DEVICES"]="0"
+from rnn_bimodel import RNNBimodel
+from text_processing import textProcessing
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.preprocessing.text import Tokenizer as Tokenizer
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint
+
+if "all_descriptions_train.zarr" in os.listdir('.') and "embedding_train_128.zarr" in os.listdir('.'):
+    with tf.device('/cpu:0'):
+        embedding_train = da.from_zarr("embedding_train_128.zarr")
+        desc_train = da.from_zarr("all_descriptions_train.zarr")
+else: 
+    print("Embedding and Descriptions dask array haven't been saved, please run text_preprocessing.py")
+    exit()
+
+if "all_descriptions_validation.zarr" in os.listdir('.') and "embedding_validation_128.zarr" in os.listdir('.'):
+    with tf.device('/cpu:0'):
+        embedding_validation = da.from_zarr("embedding_validation_128.zarr")
+        desc_validation = da.from_zarr("all_descriptions_validation.zarr")
+else: 
+    print("Embedding and Descriptions dask array haven't been saved, please run text_preprocessing.py")
+    exit()
+
+if "all_descriptions_test.zarr" in os.listdir('.') and "embedding_test_128.zarr" in os.listdir('.'):
+    with tf.device('/cpu:0'):
+        embedding_test = da.from_zarr("embedding_test_128.zarr")
+        desc_test = da.from_zarr("all_descriptions_test.zarr")
+else: 
+    print("Embedding and Descriptions dask array haven't been saved, please run text_preprocessing.py")
+    exit()
+
+allText = []
+for desc_array in [desc_train.compute(), desc_train.compute(), desc_validation.compute()]:
+    for descs in desc_array:
+        for desc in descs:
+            allText.append(desc)
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(allText)
+vocab_size = len(tokenizer.word_index) + 1
+
+max_length = max(len(d.split()) for d in allText)
+
+def count_length(tokenizer, descriptions):
+    Y = 0
+    for i in tqdm(range(len(descriptions))):
+        for j in range(len(descriptions[i])):
+            seq = tokenizer.texts_to_sequences([(descriptions[i][j].compute()).tolist()])[0]
+            Y+=len(seq)-1
+    return Y
+
+if 'best_model_rnn.h5' in os.listdir():
+    model = load_model('best_model_rnn.h5')
+    initial_epoch=initial_epoch = 0
+else:
+    model = RNNBimodel.build(vocab_size, max_length)
+    initial_epoch = 0
+
+print(model.summary())
+
+epochs = 50
+batch_size = 256
+
+# nb_train_samples = count_length(tokenizer, desc_train)
+# nb_validation_samples = count_length(tokenizer, desc_validation)
+nb_train_samples = 472734
+nb_validation_samples = 236630
+print(nb_train_samples, nb_validation_samples)
+print('max_length: {}\nvocab_size: {}\nepochs: {}'.format(max_length,vocab_size,epochs))
+with tf.device('/gpu:0'):
+    stop_early = EarlyStopping(monitor='val_loss',patience = 10)
+    ModelCheck = ModelCheckpoint('best_model_rnn.h5', monitor='val_loss', mode='min', verbose=1, save_best_only=True)
+    callbacks = [stop_early, ModelCheck]
+    train_generator = textProcessing.data_generator(desc_train, embedding_train, tokenizer, max_length, vocab_size)
+    validation_generator = textProcessing.data_generator(desc_validation, embedding_validation, tokenizer, max_length, vocab_size)
+    model.fit_generator(
+        train_generator, 
+        epochs=epochs, 
+        steps_per_epoch=nb_train_samples//batch_size,
+        validation_data = validation_generator,
+        validation_steps = nb_validation_samples//batch_size,
+        callbacks = callbacks,
+        initial_epoch=6,
+        verbose=1)
+    model
--- text_processing.py
+++ text_processing.py
@@ -33,10 +33,10 @@ class textProcessing:
     @staticmethod
     def data_generator(descriptions, photos, tokenizer, max_length, vocab_size):
         while 1:
-            for i in range(len(descriptions)):
-                in_img, in_seq, out_word = textProcessing.create_sequences(tokenizer, max_length, descriptions[i], photos[i], vocab_size)
-                nb = in_img.shape[0]
-                yield [[in_img, in_seq], out_word, nb]
+            for i in range(len(photos)):
+                for j in range(len(descriptions[i])):
+                    in_img, in_seq, out_word = textProcessing.create_sequences(tokenizer, max_length, descriptions[i][j], photos[i], vocab_size)
+                    yield ([in_img, in_seq], out_word)
     
     @staticmethod
     def word_for_id(integer, tokenizer):
@@ -68,166 +68,196 @@ class textProcessing:
         return in_text
 
 
-def convert_embedding(embedding):
-    embedding = (embedding*1000).astype(int)
-    for x in range(len(embedding)):
-        if embedding[x]>100:
-            embedding[x] = 1
-        elif embedding[x] > 50:
-            embedding[x] = 0.5
-        else:
-            embedding[x]=0
-    return embedding
+if __name__ == "__main__":
+    def convert_embedding(embedding):
+        embedding = (embedding*1000).astype(int)
+        for x in range(len(embedding)):
+            if embedding[x]>100:
+                embedding[x] = 1
+            elif embedding[x] > 50:
+                embedding[x] = 0.5
+            else:
+                embedding[x]=0
+        return embedding
 
-with open("train_captions.json") as file:
-    train_captions = js.load(file)
-with open("test_captions.json") as file:
-    test_captions = js.load(file)
-with open("validation_captions.json") as file:
-    validation_captions = js.load(file)
-    
-table = str.maketrans('', '', string.punctuation)
-for k in [train_captions, test_captions, validation_captions]:
-    caption_list = list(k.keys())
-    for i in tqdm(range(len(caption_list))):
-        cap = k[caption_list[i]]
-        list_cap = []
-        for j in range(len(cap)):
-            desc = cap[j]
-            desc = desc.split()
-            desc = [word.lower() for word in desc]
-            desc = [w.translate(table) for w in desc]
-            desc = [word for word in desc if len(word)>1]
-            desc = [word for word in desc if word.isalpha()]
-            list_cap.append(' '.join(desc))
-        k[caption_list[i]] = list_cap
+    with open("train_captions.json") as file:
+        train_captions = js.load(file)
+    with open("test_captions.json") as file:
+        test_captions = js.load(file)
+    with open("validation_captions.json") as file:
+        validation_captions = js.load(file)
+        
+    table = str.maketrans('', '', string.punctuation)
+    for k in [train_captions, test_captions, validation_captions]:
+        caption_list = list(k.keys())
+        for i in tqdm(range(len(caption_list))):
+            cap = k[caption_list[i]]
+            list_cap = []
+            for j in range(len(cap)):
+                desc = cap[j]
+                desc = desc.split()
+                desc = [word.lower() for word in desc]
+                desc = [word for word in desc if word != 'the']
+                desc = [word for word in desc if word != 'an']
+                desc = [w.translate(table) for w in desc]
+                desc = [word for word in desc if len(word)>1]
+                desc = [word for word in desc if word.isalpha()]
+                list_cap.append(' '.join(desc))
+            k[caption_list[i]] = list_cap
 
-with open("train_captions.json",'w') as file:
-    js.dump(train_captions, file)
-with open("test_captions.json",'w') as file:
-    js.dump(test_captions, file)
-with open("validation_captions.json",'w') as file:
-    js.dump(validation_captions, file)
+    with open("train_captions.json",'w') as file:
+        js.dump(train_captions, file)
+    with open("test_captions.json",'w') as file:
+        js.dump(test_captions, file)
+    with open("validation_captions.json",'w') as file:
+        js.dump(validation_captions, file)
 
-if "best_model.h5" in os.listdir():
-    cnn_model = load_model("best_model.h5")
-else: 
-    print("Please run CNN model in Image_captioning_CNN.ipynb")
-    exit()
+    if "best_model.h5" in os.listdir():
+        cnn_model = load_model("best_model.h5")
+    else: 
+        print("Please run CNN model in Image_captioning_CNN.ipynb")
+        exit()
 
-if 'all_descriptions_train.zarr' not in os.listdir('.') and 'embedding_trains_128.zarr' not in os.listdir('.'):
-    # Add a new folder which has the train images for training RNN model
-    trainImagePaths = list(paths.list_images('train'))
-    # Make sure the length is set as this for a particular folder
-    train_length = sum(len(train_captions[key.split(os.path.sep)[-1]]) for key in trainImagePaths)
-    
-    labelsList = np.load('labels.npy', allow_pickle = True)
-    embedding_train = np.zeros(shape = (train_length+1,)+labelsList.shape)
-    all_descriptions_train = []
+    if 'all_descriptions_train.zarr' not in os.listdir('.') and 'embedding_trains_128.zarr' not in os.listdir('.'):
+        # Add a new folder which has the train images for training RNN model
+        trainImagePaths = list(paths.list_images('train'))
+        train_length = 30000
+        
+        labelsList = np.load('labels.npy', allow_pickle = True)
+        embedding_train = np.zeros(shape = (1000,)+labelsList.shape)
+        all_descriptions_train = []
+        embedding_train_dask = da.from_array(np.zeros(shape=(0,80)), chunks=827)
+        done_val=0
 
-    print("Not found embeddings for train, saving it now along with descriptions...")
-    for i in tqdm(range(train_length)):
-        imagePath = trainImagePaths[i]
-        imageName = imagePath.split(os.path.sep)[-1]
-        image = Image.open(imagePath)
-        image = image.resize(size = (128,128), resample = Image.ANTIALIAS)
-        arr = np.array(image)
-        if (len(arr.shape) == 2):
-            arr = np.repeat(arr[:, :, np.newaxis], 3, axis=2)
-        arr = np.expand_dims(arr,0)
-        arr = arr/255
-        embedding = cnn_model.predict(arr)[0]
-        embedding = convert_embedding(embedding)
-        for j in range(len(train_captions[imageName])):
-            embedding_train[i+j] = embedding
-            image_desc = ''.join(train_captions[imageName][j])
-            updated_desc = 'startseq '+image_desc+' endseq'
-            all_descriptions_train.append(updated_desc)
-            break
+        print("Not found embeddings for train, saving it now along with descriptions...")
+        for i in tqdm(range(train_length)):
+            imagePath = trainImagePaths[i]
+            imageName = imagePath.split(os.path.sep)[-1]
+            image = Image.open(imagePath)
+            image = image.resize(size = (128,128), resample = Image.ANTIALIAS)
+            arr = np.array(image)
+            if (len(arr.shape) == 2):
+                arr = np.repeat(arr[:, :, np.newaxis], 3, axis=2)
+            arr = np.expand_dims(arr,0)
+            arr = arr/255
+            embedding = cnn_model.predict(arr)[0]
+            # embedding = convert_embedding(embedding)
+            embedding_train[i-done_val] = embedding
+            if (i-done_val) == 999:
+                done_val=i+1
+                # print(embedding_train.shape,embedding_train_dask.shape)
+                embedding_train_dask = da.concatenate([embedding_train_dask,embedding_train], axis = 0)
+                embedding_train = np.zeros(shape = (1000,)+labelsList.shape)
+            captions = []
+            for j in range(5):
+                image_desc = ''.join(train_captions[imageName][j])
+                updated_desc = 'startseq '+image_desc+' endseq'
+                captions.append(updated_desc)
+            all_descriptions_train.append(captions)
 
-    embedding_train = da.from_array(embedding_train, chunks = (827))
-    embedding_train.to_zarr('embedding_train_128.zarr')
+        # embedding_train = da.from_array(embedding_train, chunks = (827))
+        embedding_train_dask.to_zarr('embedding_train_128.zarr')
+        all_descriptions_train = np.array([np.array(xi) for xi in all_descriptions_train])
+        all_descriptions_train = np.array(all_descriptions_train)
+        all_descriptions_train = da.from_array(all_descriptions_train, chunks = (827, 5))
+        all_descriptions_train.to_zarr('all_descriptions_train.zarr')
 
-    all_descriptions_train = np.array(all_descriptions_train)
-    all_descriptions_train = da.from_array(all_descriptions_train, chunks = (827))
-    all_descriptions_train.to_zarr('all_descriptions_train.zarr')
+        del all_descriptions_train; del embedding_train; del train_captions; del trainImagePaths
 
-    del all_descriptions_train; del embedding_train; del train_captions; del trainImagePaths
+    if 'all_descriptions_validation.zarr' not in os.listdir('.') and 'embedding_validations_128.zarr' not in os.listdir('.'):
+        # Add a new folder which has the validation images for validationing RNN model
+        validationImagePaths = list(paths.list_images('validation'))
+        # Make sure the length is set as this for a particular folder
+        validation_length = sum(len(validation_captions[key.split(os.path.sep)[-1]]) for key in validationImagePaths[:2000])
+        validation_length = 20000
+        
+        labelsList = np.load('labels.npy', allow_pickle = True)
+        embedding_validation = np.zeros(shape = (1000,)+labelsList.shape)
+        all_descriptions_validation = []
+        embedding_validation_dask = da.from_array(np.zeros(shape=(0,80)), chunks=827)
+        k=0
 
-if 'all_descriptions_validation.zarr' not in os.listdir() and 'embedding_validations_128.zarr' not in os.listdir():
-    # Add a new folder which has the validation images for training RNN model
-    validationImagePaths = list(paths.list_images('validation'))
-    # Make sure the length is set as this for a particular folder
-    validation_length = sum(len(validation_captions[key.split(os.path.sep)[-1]]) for key in validationImagePaths)
-    
-    labelsList = np.load('labels.npy', allow_pickle = True)
-    embedding_validation = np.zeros(shape = (validation_length+1,)+labelsList.shape)
-    all_descriptions_validation = []
-    
-    print("Not found embeddings for validation, saving it now along with descriptions...")
-    for i in tqdm(range(validation_length)):
-        imagePath = validationImagePaths[i]
-        imageName = imagePath.split(os.path.sep)[-1]
-        image = Image.open(imagePath)
-        image = image.resize(size = (128,128), resample = Image.ANTIALIAS)
-        arr = np.array(image)
-        if (len(arr.shape) == 2):
-            arr = np.repeat(arr[:, :, np.newaxis], 3, axis=2)
-        arr = np.expand_dims(arr,0)
-        arr = arr/255
-        embedding = cnn_model.predict(arr)[0]
-        embedding = convert_embedding(embedding)
-        for j in range(len(validation_captions[imageName])):
-            embedding_validation[i+j] = embedding
-            image_desc = ''.join(validation_captions[imageName][j])
-            updated_desc = 'startseq '+image_desc+' endseq'
-            all_descriptions_validation.append(updated_desc)
-            break
+        print("Not found embeddings for validation, saving it now along with descriptions...")
+        for i in tqdm(range(validation_length)):
+            imagePath = validationImagePaths[i]
+            imageName = imagePath.split(os.path.sep)[-1]
+            image = Image.open(imagePath)
+            image = image.resize(size = (128,128), resample = Image.ANTIALIAS)
+            arr = np.array(image)
+            if (len(arr.shape) == 2):
+                arr = np.repeat(arr[:, :, np.newaxis], 3, axis=2)
+            arr = np.expand_dims(arr,0)
+            arr = arr/255
+            embedding = cnn_model.predict(arr)[0]
+            # embedding = convert_embedding(embedding)
+            embedding_validation[i-k] = embedding
+            if (i-k)==999:
+                    k=i+1
+                    # print(embedding_validation.shape,embedding_validation_dask.shape)
+                    embedding_validation_dask = da.concatenate([embedding_validation_dask,embedding_validation], axis = 0)
+                    embedding_validation = np.zeros(shape = (1000,)+labelsList.shape)
+            captions = []
+            for j in range(5):
+                image_desc = ''.join(validation_captions[imageName][j])
+                updated_desc = 'startseq '+image_desc+' endseq'
+                captions.append(updated_desc)
+            all_descriptions_validation.append(captions)
 
-    embedding_validation = da.from_array(embedding_validation, chunks = (827))
-    embedding_validation.to_zarr('embedding_validation_128.zarr')
+        # embedding_validation = da.from_array(embedding_validation, chunks = (827))
+        embedding_validation_dask.to_zarr('embedding_validation_128.zarr')
 
-    all_descriptions_validation = np.array(all_descriptions_validation)
-    all_descriptions_validation = da.from_array(all_descriptions_validation, chunks = (827))
-    all_descriptions_validation.to_zarr('all_descriptions_validation.zarr')
+        all_descriptions_validation = np.array([np.array(xi) for xi in all_descriptions_validation])
+        all_descriptions_validation = np.array(all_descriptions_validation)
+        all_descriptions_validation = da.from_array(all_descriptions_validation, chunks = (827, 5))
+        all_descriptions_validation.to_zarr('all_descriptions_validation.zarr')
 
-    del all_descriptions_validation; del embedding_validation; del validation_captions; del validationImagePaths
+        del all_descriptions_validation; del embedding_validation; del validation_captions; del validationImagePaths
 
-if 'all_descriptions_test.zarr' not in os.listdir() and 'embedding_tests_128.zarr' not in os.listdir():
-    # Add a new folder which has the test images for training RNN model
-    testImagePaths = list(paths.list_images('test'))
-    # Make sure the length is set as this for a particular folder
-    test_length = sum(len(test_captions[key.split(os.path.sep)[-1]]) for key in testImagePaths)
-    
-    labelsList = np.load('labels.npy', allow_pickle = True)
-    embedding_test = np.zeros(shape = (test_length+1,)+labelsList.shape)
-    all_descriptions_test = []
+    if 'all_descriptions_test.zarr' not in os.listdir('.') and 'embedding_tests_128.zarr' not in os.listdir('.'):
+        # Add a new folder which has the test images for testing RNN model
+        testImagePaths = list(paths.list_images('test'))
+        # Make sure the length is set as this for a particular folder
+        test_length = sum(len(test_captions[key.split(os.path.sep)[-1]]) for key in testImagePaths[:2000])
+        test_length = 10000
+        
+        labelsList = np.load('labels.npy', allow_pickle = True)
+        embedding_test = np.zeros(shape = (1000,)+labelsList.shape)
+        all_descriptions_test = []
+        embedding_test_dask = da.from_array(np.zeros(shape=(0,80)), chunks=827)
+        k=0
 
-    print("Not found embeddings for test, saving it now along with descriptions...")
-    for i in tqdm(range(test_length)):
-        imagePath = testImagePaths[i]
-        imageName = imagePath.split(os.path.sep)[-1]
-        image = Image.open(imagePath)
-        image = image.resize(size = (128,128), resample = Image.ANTIALIAS)
-        arr = np.array(image)
-        if (len(arr.shape) == 2):
-            arr = np.repeat(arr[:, :, np.newaxis], 3, axis=2)
-        arr = np.expand_dims(arr,0)
-        arr = arr/255
-        embedding = cnn_model.predict(arr)[0]
-        embedding = convert_embedding(embedding)
-        for j in range(len(test_captions[imageName])):
-            embedding_test[i+j] = embedding
-            image_desc = ''.join(test_captions[imageName][j])
-            updated_desc = 'startseq '+image_desc+' endseq'
-            all_descriptions_test.append(updated_desc)
-            break
+        print("Not found embeddings for test, saving it now along with descriptions...")
+        for i in tqdm(range(test_length)):
+            imagePath = testImagePaths[i]
+            imageName = imagePath.split(os.path.sep)[-1]
+            image = Image.open(imagePath)
+            image = image.resize(size = (128,128), resample = Image.ANTIALIAS)
+            arr = np.array(image)
+            if (len(arr.shape) == 2):
+                arr = np.repeat(arr[:, :, np.newaxis], 3, axis=2)
+            arr = np.expand_dims(arr,0)
+            arr = arr/255
+            embedding = cnn_model.predict(arr)[0]
+            # embedding = convert_embedding(embedding)
+            embedding_test[i-k] = embedding
+            if (i-k)==999:
+                    k=i+1
+                    # print(embedding_test.shape,embedding_test_dask.shape)
+                    embedding_test_dask = da.concatenate([embedding_test_dask,embedding_test], axis = 0)
+                    embedding_test = np.zeros(shape = (1000,)+labelsList.shape)
+            captions = []
+            for j in range(5):
+                image_desc = ''.join(test_captions[imageName][j])
+                updated_desc = 'startseq '+image_desc+' endseq'
+                captions.append(updated_desc)
+            all_descriptions_test.append(captions)
 
-    embedding_test = da.from_array(embedding_test, chunks = (827))
-    embedding_test.to_zarr('embedding_test_128.zarr')
+        # embedding_test = da.from_array(embedding_test, chunks = (827))
+        embedding_test_dask.to_zarr('embedding_test_128.zarr')
 
-    all_descriptions_test = np.array(all_descriptions_test)
-    all_descriptions_test = da.from_array(all_descriptions_test, chunks = (827))
-    all_descriptions_test.to_zarr('all_descriptions_test.zarr')
+        all_descriptions_test = np.array([np.array(xi) for xi in all_descriptions_test])
+        all_descriptions_test = np.array(all_descriptions_test)
+        all_descriptions_test = da.from_array(all_descriptions_test, chunks = (827, 5))
+        all_descriptions_test.to_zarr('all_descriptions_test.zarr')
 
-    del all_descriptions_test; del embedding_test; del test_captions; del testImagePaths
+        del all_descriptions_test; del embedding_test; del test_captions; del testImagePaths
